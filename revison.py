b84aa3c5198a72860ddc9f65ffb1fd2779846703
Sourya <souryagupta2005@gmail.com>
2025-05-10 23:25:06 +0530

    Revised Prefix Sum

--- revison.py
+++ revison.py
@@ -1,11 +1,11 @@
-# Prefix Sum 
+# # Prefix Sum 
 
-arr = [1, 2, 3, 4]
+# arr = [1, 2, 3, 4]
 
-prr = [0] * len(arr)
-prr[0] = arr[0]
+# prr = [0] * len(arr)
+# prr[0] = arr[0]
 
-for i in range(1, len(arr)) :
-    prr[i] = prr[i-1] + arr[i]
+# for i in range(1, len(arr)) :
+#     prr[i] = prr[i-1] + arr[i]
     
-print(prr)
+# print(prr)
