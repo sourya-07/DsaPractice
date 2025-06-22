e7df38434de4153297b945515c00380bc356cd94
Sourya <souryagupta2005@gmail.com>
2025-06-22 11:08:15 +0530

    Learnt the implementation of queue using array and linked list

--- stack_queue.py
+++ stack_queue.py
@@ -89,3 +89,126 @@
     
 #     def size(self):
 #         return self.length
+
+
+
+
+
+
+
+
+
+
+# Array Implementation of Queue :-
+
+# class Queue :
+#     def __init__(self) :
+#         self.q = []
+#         self.front = -1
+        
+#     def push(self, x) :
+#         if self.front == -1 :
+#             self.front = 0
+        
+#         self.q.append(x)
+        
+#     def pop(self):
+#         if len(self.q) == 0 :
+#             return -1
+#         x = self.q[self.front]
+#         self.front += 1
+#         if self.front == len(self.q) :
+#             self.front = -1
+#             self.q = []
+        
+#         return x
+    
+#     def getFront(self) :
+#         if len(self.q) == 0:
+#             return -1
+#         return self.q[self.front]
+    
+#     def size(self):
+#         if self.front == -1 :
+#             return 0
+#         return len(self.q) - self.front
+    
+    
+    
+# queue = Queue()
+# queue.push(5)
+# queue.push(4)
+# queue.push(3)
+# queue.push(1)
+
+# print(queue.getFront())
+# queue.pop()
+# print(queue.getFront())
+
+# print(queue.size())
+
+
+
+
+
+
+
+
+# Linked List Implementation of Queue :-
+
+
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+
+# class Queue:
+#     def __init__(self):
+#         self.front = None
+#         self.rear = None
+#         self.length = 0
+        
+#     def push(self, x) :
+#         newNode = Node(x)
+#         self.length += 1
+#         if self.front is None:
+#             self.front = newNode
+#             self.rear = newNode
+            
+#         else:  
+#             self.rear.next = newNode
+#             self.rear = newNode
+            
+#     def pop(self):
+#         if self.front is None:
+#             return -1
+        
+#         x = self.front.data
+#         self.front = self.front.next
+#         self.length -= 1
+#         if self.front is None:
+#             self.rear = None
+            
+#         return x
+    
+
+#     def getFront(self) :
+#         if self.front is None:
+#             return -1
+        
+#         return self.front.data
+    
+#     def size(self) :
+#         return self.length
+    
+
+# queue = Queue()
+# queue.push(1)
+# queue.push(4)
+# queue.push(3)
+# queue.push(1)
+
+# print(queue.getFront())
+# queue.pop()
+# print(queue.getFront())
+# print(queue.size())
