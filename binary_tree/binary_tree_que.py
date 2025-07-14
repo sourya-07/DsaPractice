ed23a9933eadd84899eb32e1bc2805152cdca537
Sourya <souryagupta2005@gmail.com>
2025-07-14 22:55:18 +0530

    learnt about level order traversal

---
+++ binary_tree/binary_tree_que.py
@@ -0,0 +1,42 @@
+# BFS:- by kunal khuswaha
+from collections import deque
+
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+def bfs(root):
+    result = []
+    
+    if root is None:
+        return result
+    
+    queue = deque()
+    queue.append(root)
+    
+    while queue:
+        level_size = len(queue)
+        curr_level = []
+        for i in range(level_size):
+            curr_node = queue.popleft()
+            curr_level.append(curr_node.val)
+            
+            if curr_node.left is not None:
+                queue.append(curr_node.left)
+            if curr_node.right is not None:
+                queue.append(curr_node.right)
+            
+        result.append(curr_level)
+    
+    return result
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+print(bfs(root))  # Output: [1, 2, 3, 4, 5, 6, 7]  # Level Order Traversal
