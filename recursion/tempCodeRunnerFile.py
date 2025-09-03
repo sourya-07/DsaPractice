13af6cd7124a256b4e050ee3543dfe33661a85e7
Sourya <souryagupta2005@gmail.com>
2025-09-04 01:55:44 +0530

    learnt more about back track

--- recursion/tempCodeRunnerFile.py
+++ recursion/tempCodeRunnerFile.py
@@ -1,38 +1,12 @@
 
-def all_path(paths, maze, r, c) :
-    if r == len(maze) - 1 and len(maze[0]) - 1 == c :
-        return [paths]
+def count_path(r, c) :
+    if r == 1 or c == 1 :
+        return 1
     
-    if not maze[r][c]:
-        return []
+    left = count_path(r - 1, c)
+    right = count_path(r, c - 1)
     
-    maze[r][c] = False
-    
-    down, right, left, up = [], [], [], []
-    
-    if r < len(maze) - 1 :
-        down = all_path(paths + "D", maze, r + 1, c)
-        
-    if r > 0 :
-        up = all_path(paths + "U", maze, r - 1, c)
-    
-    if c > 0 :
-        left = all_path(paths + 'L', maze, r, c - 1)
-    
-    if c < len(maze[0]) - 1 :
-        right = all_path(paths + 'R', maze, r, c + 1)
-        
-    return down + up + left + right
-
-    maze[r][c] = True
-
-board = [
-    [True, True, True], 
-    [True, True, True], 
-    [True, True, True]
-    ]
-
-print(all_path('', board, 0, 0))
+    return left + right
 
 
-
+print(count_path(3, 3))
