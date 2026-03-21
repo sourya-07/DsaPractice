073634ce9751196ac2f098aa64563ce394c1facf
Sourya <souryagupta2005@gmail.com>
2026-03-21 16:45:00 +0530

    Basic Dynamic Programming: Fibonacci and Climbing Stairs

---
+++ dp/fibo.py
@@ -0,0 +1,18 @@
+# Sol using memoization:
+# def fibonacci(dp, n):
+#     if n <= 1:
+#         return n
+    
+#     if dp[n] != -1: return dp[n]
+    
+#     dp[n] = fibonacci(dp, n-1) + fibonacci(dp, n - 2)
+#     return dp[n]
+
+# n = int(input())
+# dp = [-1] * (n + 1)
+# print(fibonacci(dp, n))
+
+
+
+# sol using Tabulation:
+
